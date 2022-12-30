249c3f580493ce1ce16d1a75b510f10db5b0d3d9
Hugo Novais <a96267@uminho.pt>
2022-12-30 11:56:15 +0000

    Acontecimentos durante a corrida e a volta

--- Entrega3/RacingManager/RacingManager/SSCarro/Carro.java
+++ Entrega3/RacingManager/RacingManager/SSCarro/Carro.java
@@ -4,6 +4,8 @@ import RacingManager.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
 
 public class Carro {
 
@@ -24,8 +26,15 @@ public class Carro {
 	private String modelo;
 	private String id;
 
-	// private Piloto piloto;  // esqueci-me disto, não deve ser mais preciso
+	private Piloto piloto;
 
+	public Piloto getPiloto() {
+		return piloto;
+	}
+
+	public void setPiloto(Piloto piloto) {
+		this.piloto = piloto;
+	}
 
 	public String getIdCarro() {
 		return idCarro;
@@ -35,53 +44,134 @@ public class Carro {
 		this.idCarro = idCarro;
 	}
 
-	/*
-        public boolean tentaUltrapassar(String GDU, Jogador frente){
-            // Alguém que faça esta merda
+	public boolean tentaUltrapassar(String gdu) {
+		boolean response = false;
+		float SVA1 = piloto.getSVA();
 
-            return false;
-        }
+		float value = (SVA1 + randomFloat()) / 2;
 
-        public boolean calculaDNF() {
-            //Outra
+		if (value >= 0.5)
+			response = true;
 
-            return false;
-        }
+		return response;
+	}
 
-        public boolean calculaOverall() {
-            //Mais Uma malta
+	public boolean calculaDNF(float v, String gdu, String meteorologia) {
+		boolean response = false;
+		float e = 0;
+		float cts = piloto.getCTS();
+		float c = calculaMeteorologia(meteorologia);
 
-            return false;
-        }
+		if(Objects.equals(gdu, "Reta"))
+			e = 0.25F;
+		else if(Objects.equals(gdu, "Curva"))
+			e = 0.5F;
+		else
+			e = 0.75F;
 
-        public void calculaTempo() {
-            // Boa Sorte
-            // Depois de calcular meter na lista de tempo :)
-        }
-    */
-	public float calculaDNF(String meteorologia){
+		if(Objects.equals(meteorologia, "Seco"))
+			cts = 1 - cts;
+
+		float value = (e + v + c + cts + (4 * randomFloat())) / 8;
+
+		if (value >= 0.5)
+			response = true;
+
+		return response;
+	}
+
+	public float calculaMeteorologia(String meteorologia){
 		float p = 0;
 		float m = 0;
 
-		if (meteorologia == "Chuva" && (pneus == "Macio" || pneus == "Duro"))
+		if (Objects.equals(meteorologia, "Chuva") && (Objects.equals(pneus, "Macio") || Objects.equals(pneus, "Duro")))
 			p = 1;
-		else if(meteorologia == "Chuva")
+		else if(Objects.equals(meteorologia, "Chuva"))
 			p = 0;
 		else{
-			if (pneus == "Macio")
+			if (Objects.equals(pneus, "Macio"))
 				p = 0.75F;
 			else
 				p = 0.25F;
 		}
 
-		if (motor == "Conservador")
+		if (Objects.equals(motor, "Conservador"))
 			p = 0.25F;
-		else if (motor == "Normal")
+		else if (Objects.equals(motor, "Normal"))
 			p = 0.5F;
 		else
 			p = 0.75F;
 
 		return (p + m + (1 - PAC) + (1 - downforce));
+	}
+	public boolean calculaOverall(Carro frente) {
+		boolean response = false;
+		float c1, c2, p1, p2, m1, m2, d1, d2, pac1, pac2 = 0;
+		d1 = getDownforce();
+		d2 = frente.getDownforce();
+		pac1 = getPAC();
+		pac2 = frente.getPAC();
+
+		String categoria1 = getCategoria();
+		String categoria2 = frente.getCategoria();
+		String pneus1 = getPneus();
+		String pneus2 = frente.getPneus();
+		String motor1 = getMotor();
+		String motor2 = frente.getMotor();
+
+		if (Objects.equals(categoria1, "C1"))
+			c1 = 1;
+		else if (Objects.equals(categoria1, "C2"))
+			c1 = 0.75F;
+		else if (Objects.equals(categoria1, "GT"))
+			c1 = 0.5F;
+		else
+			c1 = 0.25F;
+
+		if (Objects.equals(categoria2, "C1"))
+			c2 = 1;
+		else if (Objects.equals(categoria2, "C2"))
+			c2 = 0.75F;
+		else if (Objects.equals(categoria2, "GT"))
+			c2 = 0.5F;
+		else
+			c2 = 0.25F;
+
+		if (Objects.equals(pneus1, "Macio"))
+			p1 = 0.75F;
+		else if (Objects.equals(pneus1, "Duro"))
+			p1 = 0.5F;
+		else
+			p1 = 0.25F;
+
+		if (Objects.equals(pneus2, "Macio"))
+			p2 = 0.75F;
+		else if (Objects.equals(pneus2, "Duro"))
+			p2 = 0.5F;
+		else
+			p2 = 0.25F;
+
+		if (Objects.equals(motor1, "Agressivo"))
+			m1 = 0.75F;
+		else if (Objects.equals(motor1, "Normal"))
+			m1 = 0.5F;
+		else
+			m1 = 0.25F;
+
+		if (Objects.equals(motor2, "Agressivo"))
+			m2 = 0.75F;
+		else if (Objects.equals(motor2, "Normal"))
+			m2 = 0.5F;
+		else
+			m2 = 0.25F;
+
+		float val1 = (c1 / 2) + ((p1 + m1 + d1 + pac1) / 4);
+		float val2 = (c2 / 2) + ((p2 + m2 + d2 + pac2) / 4);
+
+		if (val1 > val2)
+			response = true;
+
+		return response;
 	}
 	public long somaTempo(){
 		long sum = 0;
@@ -90,6 +180,11 @@ public class Carro {
 		}
 
 		return sum;
+	}
+	private float randomFloat(){
+		Random rng = new Random();  // create a new random number generator
+
+		return rng.nextFloat();
 	}
 
 	public int getVoltas() {
--- Entrega3/RacingManager/RacingManager/SSCorrida/Corrida.java
+++ Entrega3/RacingManager/RacingManager/SSCorrida/Corrida.java
@@ -8,79 +8,126 @@ import java.util.*;
 public class Corrida {
 
 	private int voltas;
-	private List<Jogador> posicao = new ArrayList<>();
+	private List<Carro> posicao = new ArrayList<>();
 	private Map<String, List<Long>> tempo = new HashMap<>();
 	private String meteorologia;
 	private Circuito circuito;
-	private Map<String, List<Carro>> posCategoria;
+	private Map<String, List<Carro>> posCategoria = new HashMap<>();
 
 	public String simulaCorrida(){
-		List<Jogador> ultrapassar = new ArrayList<>();
-		List<Jogador> DNF = new ArrayList<>();
+		List<Carro> ultrapassar = new ArrayList<>();
+		StringBuilder acontecimentos = new StringBuilder();
 
 		for (int i = 1; i <= voltas; i++){
 			List<Elemento> elementos = circuito.getElementos();
+			int x = 0;
 
 			for (Elemento e : elementos){
+				x++;
 				String GDU = e.getGDU();
 
 				for (int j = 1; j < posicao.size(); j++){
-					Jogador c = posicao.get(j);
-					boolean res = c.tentaUltrapassar(GDU);
+					Carro c = posicao.get(j);
 
-					if(res){ultrapassar.add(c);}
+					if(!c.getDNF()) {
+						boolean res = c.tentaUltrapassar(GDU);
+						if (res) { ultrapassar.add(c); }
+					}
 				}
 
-				for(Jogador u : ultrapassar){
+				for(Carro u : ultrapassar){
 					boolean dnf = u.calculaDNF(i / voltas, GDU, meteorologia);
 
 					if(dnf){
-						u.carro.setDNF(true);
-						DNF.add(u);
+						u.setDNF(true);
+						acontecimentos.append(adicionarAcontecimento(u, e, "DNF", x));
 					}
 					else{
 						boolean ovr = false;
 						int ind = posicao.indexOf(u);
 
 						if (ind != 0)
-							u.calculaOverall(posicao.get(ind - 1));
+							ovr = u.calculaOverall(posicao.get(ind - 1));
 
-						if(ovr){updatePosicao(u);}
+						if(ovr){
+							updatePosicao(u);
+							acontecimentos.append(adicionarAcontecimento(u,e,"Ultrapassagem",x));
+						}
 					}
 				}
 
-				for (Jogador p : posicao){
+				//for (Carro p : posicao){
 					//p.carro.calculaTempo();
-					updateTempo(p);
-				}
+					//updateTempo(p);
+				//}
+
+				ultrapassar.clear();
 			}
 
-			printResumo();
+			printResumo(i, acontecimentos);
+
 		}
 
 		return resumoCorrida();
 	}
 
-	private void updatePosicao(Jogador u){
+	private void updatePosicao(Carro u){
 		int ind = posicao.indexOf(u);
 		Collections.swap(posicao, ind, ind - 1);
 	}
 
-	private void updateTempo(Jogador p){
+	private void updateTempo(Carro p){
 		List<Long> list = tempo.get(p.getId());
-		list.add(p.carro.somaTempo());
+		list.add(p.somaTempo());
 	}
 
-	private void printResumo(){
-		//Tentativa de Ultrapassagem
-		//Ultrapassagens
-		//DNF
-		//Tempo e Volta
+	private String adicionarAcontecimento(Carro u, Elemento e, String acontecimento, int numero){
+		if (Objects.equals(acontecimento, "DNF")){
+			return "O piloto " + u.getPiloto().getNomePiloto() + " despistou-se na " + e.getCategoria() + " " + numero + "\n";
+		}
+		else if (Objects.equals(acontecimento, "Ultrapassagem")){
+			return "O piloto " + u.getPiloto().getNomePiloto() + " fez uma ultrapassagem com sucesso na " + e.getCategoria() + " " + numero +  " e passou para " + (posicao.indexOf(u) + 1) + "\n";
+		}
+		else
+			return null;
+	}
+
+	private void printResumo(int volta, StringBuilder acontecimentos){
+		System.out.println("---------- Volta " + volta + " ----------");
+		System.out.println(acontecimentos);
+		System.out.println("------------------------------");
 	}
 
 	private String resumoCorrida(){
-		//Resumo da corrida
+		StringBuilder sb = new StringBuilder();
 
-		return null;
+		for(int i = 1; i <= posicao.size(); i++){
+			sb.append(i + "º ---> " + posicao.get(i-1));
+			sb.append("\n");
+		}
+
+		posicaoCategoria();
+
+		for (String categoria : posCategoria.keySet()) {
+			sb.append(categoria);
+			sb.append(": ");
+			for (Carro carro : posCategoria.get(categoria)) {
+				sb.append(carro.getPiloto().getNomePiloto());
+				sb.append(", ");
+			}
+			sb.append("\n");
+		}
+
+		return sb.toString();
+	}
+
+	private void posicaoCategoria(){
+		for (Carro carro : posicao) {
+			String categoria = carro.getCategoria();
+			if (!posCategoria.containsKey(categoria)) {
+				posCategoria.put(categoria, new ArrayList<>());
+			}
+			posCategoria.get(categoria).add(carro);
+		}
 	}
 }
